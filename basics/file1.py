e756006ac0b4a89c4cc7a90aaf575e6b28f0a566
blackadderIII <skobarnes1@gmail.com>
2024-06-23 11:50:27 +0000

    starting with basics

---
+++ basics/file1.py
@@ -0,0 +1,7 @@
+name =  input('Please Enter Your Name\n')
+
+if name == 'Samuel' :
+        print('hello ' + name)
+
+else :
+        print('You were meant to destroy the sith not JOIN THEMMMm!!\n What even is a ' + name)
--- project1/file1.py
+++
@@ -1,7 +0,0 @@
-name =  input('Please Enter Your Name')
-
-if name == 'Samuel' :
-        print('hello ' + name)
-
-else :
-        print('You were meant to destroy the sith not JOIN THEMMMm!!')
